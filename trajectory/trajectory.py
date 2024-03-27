dc3db289144e9d67f56901255630f9819515d60e
nikhil-sharma710 <98418670+nikhil-sharma710@users.noreply.github.com>
2024-03-27 15:40:28 -0500

    Changed units

--- trajectory/trajectory.py
+++ trajectory/trajectory.py
@@ -8,7 +8,7 @@ bearing = 0  # deg (0 - N, 90 - E, 180 - S, 270 - W)
 vertical_speed = 2  # m/s
 aircraft_velocity = Vector3(air_speed*sin(radians(bearing)), air_speed*cos(radians(bearing)), vertical_speed)
 
-altitude = 150/3.28084  # ft
+altitude = 150/3.28084  # m
 
 wind_speed = 0
 wind_direction = 180 
@@ -19,7 +19,7 @@ payload_velocity = aircraft_velocity + wind_velocity
 g = -9.81  # m/s^2
 
 t = (-vertical_speed - sqrt(vertical_speed**2 - 4*altitude*g/2))/g
-displacement = Vector3(payload_velocity.x*t*3.28084, payload_velocity.y*t*3.28084, -150)  # Standardized to feet
+displacement = Vector3(payload_velocity.x*t*3.28084, payload_velocity.y*t*3.28084, -150)  # Standardized to ft
 
 drop_coordinate = target_coordinate - displacement
 
