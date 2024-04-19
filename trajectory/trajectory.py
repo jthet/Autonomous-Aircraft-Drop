cfa229fc5caea1bfec215cd74dc9a127c0f84f7c
Sydney Loats <sydneyloats@gmail.com>
2024-04-19 13:05:53 -0500

    wind speed array

--- trajectory/trajectory.py
+++ trajectory/trajectory.py
@@ -16,6 +16,9 @@ def extract_param(parameter_name):
         break
   return parameter_value
 
+
+# initial conditions
+
 target_coordinate = Vector3(0, 0, 0)
 # target_coordinate = Vector3(extract_param('GPS_POS1_X'), extract_param('GPS_POS1_Y'), extract_param('GPS_POS1_Z'))
 
@@ -40,10 +43,30 @@ ground_speed = 0
 ground_direction = 180
 ground_altitude = 3  # m
 
-alpha_x = (log(wind_speed*cos(radians(wind_direction))) - log(ground_speed*cos(radians(ground_direction))))/(log(altitude) - log(ground_altitude))
-alpha_y = (log(wind_speed*sin(radians(wind_direction))) - log(ground_speed*sin(radians(ground_direction))))/(log(altitude) - log(ground_altitude))
+alpha_x = (log(wind_speed*sin(radians(wind_direction))) - log(ground_speed*sin(radians(ground_direction))))/(log(altitude) - log(ground_altitude))  # shear coeff for wind in x
+alpha_y = (log(wind_speed*cos(radians(wind_direction))) - log(ground_speed*cos(radians(ground_direction))))/(log(altitude) - log(ground_altitude))  # shear coeff for wind in y
 
-t = (-vertical_speed - sqrt(vertical_speed**2 - 4*altitude/3.28084*g/2))/g
+# time array
+time_array = []
+altitude_array = []
+for i in range(150, 0, -10):
+  time_array.append((-vertical_speed - sqrt(vertical_speed**2 - 4*(altitude - i)/3.28084*g/2))/g)
+  altitude_array.append(i)
+time_array.append(t)
+
+print(time_array)
+
+# wind speed
+wind_array_x = []
+wind_array_y = []
+for alt in altitude_array:
+  wind_array_x.append(wind_gradient(ground_speed*sin(radians(ground_direction)), ground_altitude, alt, alpha_x))
+  wind_array_y.append(wind_gradient(ground_speed*cos(radians(ground_direction)), ground_altitude, alt, alpha_y))
+
+print(wind_array_x)
+print(wind_array_y)
+
+t = (-vertical_speed - sqrt(vertical_speed**2 - 4*altitude/3.28084*g/2))/g  # time for payload to hit ground
 displacement = Vector3(payload_velocity.x*t*3.28084, payload_velocity.y*t*3.28084, -150)  # Standardized to ft
 
 drop_coordinate = target_coordinate - displacement
