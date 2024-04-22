a315380d9b4b50c952e2166667e4ad518997d413
Sydney Loats <sydneyloats@gmail.com>
2024-04-22 13:24:05 -0500

    clean up

--- trajectory/telem.py
+++ trajectory/telem.py
@@ -1,34 +1,15 @@
 from pymavlink import mavutil, mavwp
 
-# Set the connection parameters (change accordingly)
-connection_string = 'udp:127.0.0.1:14550' # sim connection
+# Set the connection parameters
+connection_string = '/dev/ttyS0'
 
-# Connect to the Pixhawk
+# Connect to Pixhawk
 master = mavutil.mavlink_connection(connection_string)
 
 # Receive telemetry data from pixhawk to jetson
 def receive_telem():
-    #message
-    msg = master.recv_match(type=['GLOBAL_POSITION_INT'],blocking=True)             
-    # Check if message is not None
-    if msg:
-        print("Global Position: Lat={}, Lon={}, Alt={}".format(msg.lat, msg.lon, msg.alt))        
-        # Sleep for a short duration to avoid busy-waiting
-        time.sleep(0.1)
-        
-        return msg
-
-
-# # Send telemetry data to pixhawk from jetson
-# def send_telem(coords, phase):
-#     MAX_BANK_ANGLE = 45  #Maximum allowable bank angle in degrees
-#     #Convert maximum bank angle to radians for MAVLink parameter
-#     max_bank_angle_rad = MAX_BANK_ANGLE * (math.pi / 180) #Maximum allowable bank angle in radians
-
-#     altitude = altitude_handle(phase)
-#     # Send telemetry data to Pixhawk
-
-#     master.mav.mission_item_send(
-#         master.target_system, master.target_component, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
-#         mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, max_bank_angle_rad, 0, 0,
-#         coords['latitude'], coords['longitude'], altitude)
+    # Get gps coordinates
+    params = master.recv_match(type=['GLOBAL_POSITION_INT'],blocking=True)             
+    if params:
+        print("Global Position: Lat={}, Lon={}, Alt={}".format(params.lat, params.lon, params.alt))      
+        return params
