904b8b1cb512df267c030b6c86a0be502aa631c8
nikhil-sharma710 <98418670+nikhil-sharma710@users.noreply.github.com>
2024-04-22 12:59:46 -0500

    Update trajectory.py

--- trajectory/trajectory.py
+++ trajectory/trajectory.py
@@ -48,24 +48,19 @@ def calculate_drop_coordinate():
     ground_direction = 0
     ground_altitude = 0.000001  # m
 
-    alpha_y = (log(abs(wind_velocity.y)) - log(abs(ground_speed*cos(radians(ground_direction)))))/(log(altitude) - log(ground_altitude))  # shear coeff for wind in y
+    alpha = (log(abs(wind_velocity.y)) - log(abs(ground_speed*cos(radians(ground_direction)))))/(log(altitude) - log(ground_altitude))  # shear coeff for wind in y
 
     time = (-vertical_speed - sqrt(vertical_speed**2 - 4*altitude/3.28084*g/2))/g  # time for payload to hit ground
     displacement = Vector3(payload_velocity.x*time*3.28084, payload_velocity.y*time*3.28084, -150)  # Standardized to ft
 
     # time array
-    time_array = []
-    altitude_array = []
+    times = []
+    wind_speeds = []
     for z in range(150, 0, -10):
-        time_array.append((-vertical_speed - sqrt(vertical_speed**2 - 4*(altitude - z)/3.28084*g/2))/g)
-        altitude_array.append(z)
-
-    # wind speed
-    wind_array = []
-    for z in altitude_array:
-        wind_array.append(wind_gradient(ground_speed*cos(radians(ground_direction)), ground_altitude, z, alpha_y))
+        times.append((-vertical_speed - sqrt(vertical_speed**2 - 4*(altitude - z)/3.28084*g/2))/g)
+        wind_speeds.append(wind_gradient(ground_speed*cos(radians(ground_direction)), ground_altitude, z, alpha_y))
 
     drop_coordinate = target_coordinate - displacement
-    drop_coordinate.y -= trapezoidal(time_array, wind_array)*cos(wind_direction)
+    drop_coordinate.y -= trapezoidal(times, wind_speeds)*cos(wind_direction)
 
     return drop_coordinate
