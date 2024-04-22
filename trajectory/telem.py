63c7d8ef3f9f69b1912fd81117b61be17231942b
Sydney Loats <sydneyloats@gmail.com>
2024-04-22 14:13:00 -0500

    main

--- trajectory/telem.py
+++ trajectory/telem.py
@@ -1,15 +1,27 @@
 from pymavlink import mavutil, mavwp
 
 # Set the connection parameters
-connection_string = '/dev/ttyS0'
+connection_string = '/dev/ttyUSB0'
 
 # Connect to Pixhawk
 master = mavutil.mavlink_connection(connection_string)
 
 # Receive telemetry data from pixhawk to jetson
 def receive_telem():
+    '''
+    Receive telemetry data from pixhawk to jetson.
+    '''
     # Get gps coordinates
     params = master.recv_match(type=['GLOBAL_POSITION_INT'],blocking=True)             
     if params:
         print("Global Position: Lat={}, Lon={}, Alt={}".format(params.lat, params.lon, params.alt))      
         return params
+
+def main():
+    '''
+    Main function.
+    '''
+    receive_telem()
+
+if __name__ == "__main__":
+    main()
