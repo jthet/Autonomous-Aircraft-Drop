10ce277f8faf8af697e7c77d853aaddb30579289
nikhil-sharma710 <98418670+nikhil-sharma710@users.noreply.github.com>
2024-04-22 12:57:00 -0500

    Update trajectory.py

--- trajectory/trajectory.py
+++ trajectory/trajectory.py
@@ -9,81 +9,63 @@ def wind_gradient(v1, z1, z2, alpha):
 def trapezoidal(x, y):
     area = 0
     for i in range(len(x) - 1):
-        # Calculate the width of the trapezoid
         base = x[i + 1] - x[i]
+        height = (y[i] + y[i + 1])/2
         
-        # Calculate the average height of the trapezoid
-        height = (y[i] + y[i + 1]) / 2
-        
-        # Calculate the area of the trapezoid and add it to the total area
-        area += base * height
+        area += base*height
         
     return area
 
-def extract_param(parameter_name):
-  file_name = 'mav.parm'
-  parameter_value = None
-  with open(file_name, 'r') as file:
-    for line in file:
-      parts = line.split()
-      if len(parts) > 1 and parts[0] == parameter_name:
-        parameter_value = parts[1]
-        break
-  return parameter_value
+def calculate_drop_coordinate():
+    '''
+    Variables needed from Pixhawk:
+      target_coordinate - from GPS and converted to standardized coordinates
+      air_speed
+      bearing
+      vertical_speed
+      altitude
+      wind_speed
+      wind_direction
+    '''
+    target_coordinate = Vector3(0, 0, 0)
 
-# initial conditions
+    air_speed = 20  # m/s
+    bearing = 37  # deg (0 - N, 90 - E, 180 - S, 270 - W)
+    vertical_speed = 2  # m/s
+    aircraft_velocity = Vector3(air_speed*sin(radians(bearing)), air_speed*cos(radians(bearing)), vertical_speed)
 
-target_coordinate = Vector3(0, 0, 0)
-# target_coordinate = Vector3(extract_param('GPS_POS1_X'), extract_param('GPS_POS1_Y'), extract_param('GPS_POS1_Z'))
+    altitude = 150  # ft
 
-air_speed = 20  # m/s
-# air_speed = extract_param('TRIM_ARSPD_CM')
+    wind_speed = 2
+    wind_direction = 0  # Should be 0 or 180 (head or tail wind) 
+    wind_velocity = Vector3(wind_speed*sin(radians(wind_direction)), wind_speed*cos(radians(wind_direction)), 0)
 
-bearing = 0  # deg (0 - N, 90 - E, 180 - S, 270 - W)
-vertical_speed = 2  # m/s
-aircraft_velocity = Vector3(air_speed*sin(radians(bearing)), air_speed*cos(radians(bearing)), vertical_speed)
+    payload_velocity = aircraft_velocity
 
-altitude = 150  # ft
+    g = -9.81  # m/s^2
 
-wind_speed = 2
-wind_direction = 0 
-wind_velocity = Vector3(wind_speed*sin(radians(wind_direction)), wind_speed*cos(radians(wind_direction)), 0)
+    ground_speed = 1
+    ground_direction = 0
+    ground_altitude = 0.000001  # m
 
-payload_velocity = aircraft_velocity
+    alpha_y = (log(abs(wind_velocity.y)) - log(abs(ground_speed*cos(radians(ground_direction)))))/(log(altitude) - log(ground_altitude))  # shear coeff for wind in y
 
-g = -9.81  # m/s^2
+    time = (-vertical_speed - sqrt(vertical_speed**2 - 4*altitude/3.28084*g/2))/g  # time for payload to hit ground
+    displacement = Vector3(payload_velocity.x*time*3.28084, payload_velocity.y*time*3.28084, -150)  # Standardized to ft
 
-ground_speed = 1
-ground_direction = 0
-ground_altitude = 0.000001  # m
+    # time array
+    time_array = []
+    altitude_array = []
+    for z in range(150, 0, -10):
+        time_array.append((-vertical_speed - sqrt(vertical_speed**2 - 4*(altitude - z)/3.28084*g/2))/g)
+        altitude_array.append(z)
 
-v2_x = abs(wind_speed*sin(radians(wind_direction)))
-v1_x = abs(ground_speed*sin(radians(ground_direction)))
-v2_y = abs(wind_speed*cos(radians(wind_direction)))
-v1_y = abs(ground_speed*cos(radians(ground_direction)))
+    # wind speed
+    wind_array = []
+    for z in altitude_array:
+        wind_array.append(wind_gradient(ground_speed*cos(radians(ground_direction)), ground_altitude, z, alpha_y))
 
-#alpha_x = (log(v2_x) - log(v1_x))/(log(altitude) - log(ground_altitude))  # shear coeff for wind in x
-alpha_y = (log(v2_y) - log(v1_y))/(log(altitude) - log(ground_altitude))  # shear coeff for wind in y
+    drop_coordinate = target_coordinate - displacement
+    drop_coordinate.y -= trapezoidal(time_array, wind_array)*cos(wind_direction)
 
-t = (-vertical_speed - sqrt(vertical_speed**2 - 4*altitude/3.28084*g/2))/g  # time for payload to hit ground
-displacement = Vector3(payload_velocity.x*t*3.28084, payload_velocity.y*t*3.28084, -150)  # Standardized to ft
-
-# time array
-time_array = []
-altitude_array = []
-for i in range(150, 0, -10):
-    time_array.append((-vertical_speed - sqrt(vertical_speed**2 - 4*(altitude - i)/3.28084*g/2))/g)
-    altitude_array.append(i)
-
-# wind speed
-wind_array_x = []
-wind_array_y = []
-for alt in altitude_array:
-    #wind_array_x.append(wind_gradient(ground_speed*sin(radians(ground_direction)), ground_altitude, alt, alpha_x))
-    wind_array_y.append(wind_gradient(ground_speed*cos(radians(ground_direction)), ground_altitude, alt, alpha_y))
-
-drop_coordinate = target_coordinate - displacement
-
-drop_coordinate.y -= trapezoidal(time_array, wind_array_y)*cos(wind_direction)
-
-print(drop_coordinate)
+    return drop_coordinate
