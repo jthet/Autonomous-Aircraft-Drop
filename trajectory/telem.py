c5c724602b3951ff947734e2d993df31380ce18c
Sydney Loats <sydneyloats@gmail.com>
2024-04-26 15:11:25 -0500

    send telem function

--- trajectory/telem.py
+++ trajectory/telem.py
@@ -4,17 +4,7 @@ from pymavlink from mavwp, mavutil
 master = mavutil.mavlink_connection(connection_string, baud=baudrate)
 wp = mavwp.MAVWPLoader()
 
-#*******Tested and Working*******  
-def altitude_handle(phase):
-    '''
-    handles the altitude inputs to the plane
-    '''
-    #Altitudes are in meters
-    if phase == 'search':
-        return 91.44
-    if phase == 'surveillance':
-        return 45.72
-      
+#*******Tested and Working*******       
 def receive_GLOBAL_POSITION_INT():
     '''
     receives data from the pixhawk
@@ -23,22 +13,33 @@ def receive_GLOBAL_POSITION_INT():
     if msg:
         return msg
 
-def send_GLOBAL_POSITION_INT(lat, lon, alt):
+def send_GLOBAL_POSITION_INT(waypoints):
     '''
     sends gps coordinates back to pixhawk
     '''
-    # send message
-    master.mav.mission_item_send(
-        master.target_system, master.target_component, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
-        mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0,
-        lat, lon, alt)
+    wp = mavwp.MAVWPLoader()   
+    altitude = 45.72                                                 
+    seq = 1
+    frame = mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT
+    radius = 10
+    N = len(waypoints)
+    for i in range(N):                  
+        wp.add(mavutil.mavlink.MAVLink_mission_item_message(master.target_system,
+                    master.target_component,
+                    seq,
+                    frame,
+                    mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
+                    0, 0, 0, radius, 0, 0,
+                    waypoints[i]['lat'],waypoints[i]['lon'], altitude))
+        seq += 1                                                                                  
 
-    # send waypoints
-    # msg = master.mav.mission_item_message(
-    #     master.target_system, master.target_component, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
-    #     mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0,
-    #     lat, lon, alt)
-    # master.mav.send(msg)
+    master.waypoint_clear_all_send()                                     
+    master.waypoint_count_send(wp.count())                          
+
+    for i in range(wp.count()):
+        msg = master.recv_match(type=['MISSION_REQUEST'],blocking=True)             
+        master.mav.send(wp.wp(msg.seq))                                                                      
+        print(f'Sending waypoint {msg.seq}')    
 
 def receive_WIND():
     msg = master.recv_match(type=['WIND'], blocking=True)
@@ -62,8 +63,6 @@ def main():
             row = [pos.lat, pos.lon, pos.alt, pos.vx, pos.vy, pos.vz, wind.speed, wind.direction]  # Create row of data
             writer.writerow(row)  # Write row to CSV file
         time.sleep(5)
-
-    send_GLOBAL_POSITION_INT(0, 1, 1)
 
 if __name__ == "__main__":
     main()
